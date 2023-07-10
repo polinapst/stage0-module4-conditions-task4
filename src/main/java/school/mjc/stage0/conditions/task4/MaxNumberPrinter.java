97c0448e89a64dd0a3f530b3f6f545e979375a44
polina <polinatuzankina@gmail.com>
2023-07-10 20:48:01 +0600

    module 4 task 4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first > second) {
+            System.out.println(first > third ? first : third);
+        } else {
+            System.out.println(second > third ? second : third);
+        }
     }
 }
